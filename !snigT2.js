98ad59803e96f3ae098f5e7dddcef4e322859149
Snigdoh <165558828+Snigdoh@users.noreply.github.com>
2026-09-07 11:57:53 -0700

    idx + T2.js for virtual Keyboard!
    7sep26

--- !snigT2.js
+++ !snigT2.js
@@ -1161,3 +1161,23 @@
 		document.getElementById("NOpl").value="";
 		document.getElementById("divideRESULT").value="";
 	}
+	
+	
+if ('virtualKeyboard' in navigator) {
+				alert("virtualKeyboard' IS in navigator!");
+  // Prevent the browser from automatically handling the keyboard layout
+  navigator.virtualKeyboard.overlaysContent = true;
+
+  document.getElementById('editBtn').addEventListener('click', () => {
+    document.getElementById('myEditor').focus();
+    
+    // Manually force the keyboard to slide up
+    navigator.virtualKeyboard.show();
+  });
+} else {
+				alert("virtualKeyboard' is NOT in navigator!");
+  // Fallback for browsers that don't support the VirtualKeyboard API (like iOS Safari)
+  document.getElementById('editBtn').addEventListener('click', () => {
+    document.getElementById('myEditor').focus();
+  });
+}	
